658c053636483ad031066040bb8f2c392df78caa
Santhosh <58128088+elintrovertido@users.noreply.github.com>
2022-06-18 23:59:20 +0530

    show_wifi_passwords.py

--- wifi_hack/show_wifi.py
+++ wifi_hack/show_wifi.py
@@ -1,6 +1,21 @@
+# el_introvertid0
 import subprocess
 
-# listOfFiles = subprocess.run("ls -la")
-listOfFiles = subprocess.run(['ls','-la'],capture_output=True,text=True)
+# all wlan profiles
+wifiProfiles = subprocess.check_output(["netsh", "wlan", "show", "profiles"]).decode("utf-8").split("\n")
+profiles = [i.split(":")[1][1:-1] for i in wifiProfiles if "All User Profile" in i]
 
-print(listOfFiles.stdout)
+#storing passwords in array
+wlanPasswords = []
+for i in profiles:
+    passwords = (subprocess
+    .check_output(["netsh",'wlan','show','profiles',i,'key=clear'])
+    .decode("utf-8")
+    .split("\n")
+    )
+    password = [b.split(":")[1][1:-1] for b in passwords if "Key Content" in  b]
+    wlanPasswords.append(password)
+
+#passwords output
+for i in range(len(profiles)):
+    print(profiles[i]," ==> ",*wlanPasswords[i])
